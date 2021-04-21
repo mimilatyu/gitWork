ded0c9c2c4ff8f17768d0545ae687ccb6a5a76c8
mimilatyu <dimatusyan@gmail.com>
2021-04-21 12:32:25 +0300

    branch2

--- src/Car.java
+++ src/Car.java
@@ -7,6 +7,7 @@ public class Car {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello");
+        System.out.println("Switched to branch2");
     }
 }
 
